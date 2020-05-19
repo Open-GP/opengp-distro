5af1d5aa05ea523a9596d2d4a4a8a5ca024e164b
Mircea M <mircea.moise@gmail.com>
2020-05-19 19:35:22 +0100

    Mircea: Debugging message first

--- build-docker-image-for-distro.sh
+++ build-docker-image-for-distro.sh
@@ -1,9 +1,9 @@
-#mvn openmrs-sdk:build-distro -B -Ddir=docker
 echo "-----"
 cat ~/.m2/settings.xml
 echo "-----"
 version=$(./get-version.sh)
 
+mvn openmrs-sdk:build-distro -B -Ddir=docker
 docker build docker/web -t opengp/opengp:$version
 docker tag opengp/opengp:$version 270649891125.dkr.ecr.eu-west-2.amazonaws.com/opengp:$version
 
